da5d0a7cf5cdcf2e62ef2bf3c01340ac624ef87e
Mariano Diaz <lth.nano@gmail.com>
2023-11-07 13:59:04 -0300

    change ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,8 +9,8 @@ module.exports = {
       key: "bach-dev.pem", // pem file generated when launching an instance
       user: "ubuntu", // ec2-user if you use aws linux kernel, ubuntu if you use ubuntu kernel
       host: "ec2-52-90-226-47.compute-1.amazonaws.com", // public ip address of the ec2 instance
-      ref: "main", // specify the branch where your code resides
-      repo: "https://github.com/Diaznano/star-wars-frontend", //ssh git url of your repo 
+      ref: "origin/main", // specify the branch where your code resides
+      repo: "git@github.com:Diaznano/star-wars-frontend.git", //ssh git url of your repo 
       path: "/home/ubuntu/star-wars-frontend", // mention the path in ec2 instance where your code need to be eg /home/ec2-user
       "pre-deploy-local": "",
       "post-deploy":
